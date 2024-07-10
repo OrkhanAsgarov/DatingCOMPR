22c2ff5390ae0ce8ab5da822fefcf5e8c22f85b1
OrkhanAsgarov <asgarov.orkhan89@gmail.com>
2024-07-10 12:17:47 +0300

    added gif pictures for mobile view

--- js/index.js
+++ js/index.js
@@ -1,34 +1,44 @@
 function toggleMenu() {
-  const menu = document.getElementById('dropdown-menu');
-  const icon = document.querySelector('.location-button .icon');
-  if (menu.classList.contains('show')) {
-      menu.classList.remove('show');
-      icon.classList.remove('icon-rotate');
-  } else {
-      menu.classList.add('show');
-      icon.classList.add('icon-rotate');
-  }
+    const menu = document.getElementById('dropdown-menu');
+    const icon = document.querySelector('.location-button .icon');
+    const hoverMobiles = document.querySelectorAll('.hover-mobile');
+
+    if (menu.classList.contains('show')) {
+        menu.classList.remove('show');
+        icon.classList.remove('icon-rotate');
+        if (window.innerWidth <= 750) { // Mobil görünüm kontrolü
+            hoverMobiles.forEach(element => {
+                element.style.display = 'none';
+            });
+        }
+    } else {
+        menu.classList.add('show');
+        icon.classList.add('icon-rotate');
+        if (window.innerWidth <= 750) { // Mobil görünüm kontrolü
+            hoverMobiles.forEach(element => {
+                element.style.display = 'block';
+            });
+        }
+    }
 }
 
-
 document.addEventListener('DOMContentLoaded', () => {
-  const hoverTriggers = document.querySelectorAll('.hover-trigger');
-  let hoverTimeout;
+    const hoverTriggers = document.querySelectorAll('.hover-trigger');
+    let hoverTimeout;
 
-  hoverTriggers.forEach(trigger => {
-      const hoverImage = trigger.nextElementSibling; // Bir sonraki eleman hover image olmalı
-      trigger.addEventListener('mouseover', () => {
-          clearTimeout(hoverTimeout);
-          const imgUrl = trigger.getAttribute('data-hover');
-          hoverImage.style.backgroundImage = `url(${imgUrl})`;
-          hoverImage.style.display = 'block';
-      });
+    hoverTriggers.forEach(trigger => {
+        const hoverImage = trigger.nextElementSibling; // Bir sonraki eleman hover image olmalı
+        trigger.addEventListener('mouseover', () => {
+            clearTimeout(hoverTimeout);
+            const imgUrl = trigger.getAttribute('data-hover');
+            hoverImage.style.backgroundImage = `url(${imgUrl})`;
+            hoverImage.style.display = 'block';
+        });
 
-      trigger.addEventListener('mouseout', () => {
-          hoverTimeout = setTimeout(() => {
-              hoverImage.style.display = 'none';
-          }, 100); // 100ms gecikme ekleyin
-      });
-  });
+        trigger.addEventListener('mouseout', () => {
+            hoverTimeout = setTimeout(() => {
+                hoverImage.style.display = 'none';
+            }, 100); // 100ms gecikme ekleyin
+        });
+    });
 });
-
