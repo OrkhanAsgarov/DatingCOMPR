e34c559f9090854eca9b06c36ff50c749bd18fba
OrkhanAsgarov <asgarov.orkhan89@gmail.com>
2024-07-25 14:31:39 +0300

    meetbliss page now  is now functional

--- js/meetBliss.js
+++ js/meetBliss.js
@@ -1,11 +1,48 @@
-// const noteText = document.querySelector(".noteText");
+const container = document.querySelector(".container");
+const hamburgerMenu = document.querySelector(".hamburgerMenu");
+const mobilMenuContainer = document.querySelector(".mobilMenuContainer");
+const closeMenu = document.querySelector(".mobilMenuContainer .closeMenu");
+const submitButton = document.querySelector(".noteBtnContainer button");
+const leaveContainer = document.querySelector(".leaveContainer");
+const succesContainer = document.querySelector(".succesContainer");
+const nameInput = document.querySelector(".name");
+const noteText = document.querySelector(".noteText");
+const noteBtnContainer = document.querySelector(".noteBtnContainer");
+const recentNotes = document.querySelector(".recentNotes");
 
-// if (window.innerWidth > 750) {
-//   console.log(window.innerWidth);
-//   noteText.style.width = "467px";
-//   noteText.style.height = "148px";
-// } else {
-//   console.log(window.innerWidth);
-//   noteText.style.width = "343px";
-//   noteText.style.height = "49px";
-// }
+
+
+
+hamburgerMenu.addEventListener("click", function () {
+  container.style.display = 'none';
+  mobilMenuContainer.style.display = 'flex';
+});
+
+closeMenu.addEventListener("click", function () {
+  container.style.display = 'flex';
+  mobilMenuContainer.style.display = 'none';
+});
+
+if (window.innerWidth <= 750) {
+  function toggleButtonState() {
+    if (nameInput.value.length > 0 && noteText.value.length > 0) {
+      noteBtnContainer.classList.add('active');
+    } else {
+      noteBtnContainer.classList.remove('active');
+    }
+  }
+
+  nameInput.addEventListener("input", toggleButtonState);
+  noteText.addEventListener("input", toggleButtonState);
+}
+
+submitButton.addEventListener("click", function () {
+  if(nameInput.value.length > 0 && noteText.value.length > 0){
+    leaveContainer.style.display = 'none';
+    succesContainer.style.display = 'flex';
+    recentNotes.innerHTML += nameInput.value + ": " + noteText.value;
+  } 
+  
+})
+
+
