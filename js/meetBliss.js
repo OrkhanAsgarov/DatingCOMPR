4f70b766e6e2590252ce5d16a06c141aff7f7b3c
OrkhanAsgarov <asgarov.orkhan89@gmail.com>
2024-07-25 12:01:38 +0300

    meetBliss page added

---
+++ js/meetBliss.js
@@ -0,0 +1,11 @@
+// const noteText = document.querySelector(".noteText");
+
+// if (window.innerWidth > 750) {
+//   console.log(window.innerWidth);
+//   noteText.style.width = "467px";
+//   noteText.style.height = "148px";
+// } else {
+//   console.log(window.innerWidth);
+//   noteText.style.width = "343px";
+//   noteText.style.height = "49px";
+// }
