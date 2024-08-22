a748d40b76ab12ce7c1900486cdf8fcf99241cd8
Orkhan Asgarov <asgarov.orkhan89@gmail.com>
2024-08-22 12:07:19 +0300

    welcome pages added

---
+++ js/welcome.js
@@ -0,0 +1,18 @@
+const container = document.querySelector(".container");
+const hamburgerMenu = document.querySelector(".hamburgerMenu");
+const mobilMenuContainer = document.querySelector(".mobilMenuContainer");
+const closeMenu = document.querySelector(".mobilMenuContainer .closeMenu");
+
+hamburgerMenu.addEventListener("click", function () {
+  container.style.display = "none";
+  mobilMenuContainer.style.display = "flex";
+});
+
+closeMenu.addEventListener("click", function () {
+  container.style.display = "flex";
+  mobilMenuContainer.style.display = "none";
+});
+
+
+
+
